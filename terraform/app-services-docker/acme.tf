879323d295ceefe1d2e8a4311af3c1f331c0e542
Fractiunate// David Rahäuser <david.rahaeuser@gebit.de>
2025-06-11 12:32:18 +0200

    fix: acme ifs

--- terraform/app-services-docker/acme.tf
+++ terraform/app-services-docker/acme.tf
@@ -1,16 +1,16 @@
 resource "tls_private_key" "private_key" {
-  count     = var.custom_domain != null && var.certificate_blob_b64 != null ? 0 : 1
+  count     = var.custom_domain != null && var.certificate_blob_b64 == null ? 1 : 0
   algorithm = "RSA"
 }
 
 resource "acme_registration" "reg" {
-  count           = var.custom_domain != null && var.certificate_blob_b64 != null ? 0 : 1
+  count           = var.custom_domain != null && var.certificate_blob_b64 == null ? 1 : 0
   account_key_pem = tls_private_key.private_key[0].private_key_pem
   email_address   = "hostmaster@fractiunate.de"
 }
 
 resource "acme_certificate" "certificate" {
-  count              = var.custom_domain != null && var.certificate_blob_b64 != null ? 0 : 1
+  count              = var.custom_domain != null && var.certificate_blob_b64 == null ? 1 : 0
   account_key_pem    = acme_registration.reg[0].account_key_pem
   common_name        = "${var.custom_domain.subdomain}.${var.custom_domain.domain_name}"
   min_days_remaining = 90
