6afdacf224d0b18332ced377ff2c0e5f2e7857e4
Fractiunate// David Rahäuser <david.rahaeuser@gebit.de>
2025-06-17 00:10:33 +0200

    fix: cert

--- terraform/kubernetes/tools/cert-manager.tf
+++ terraform/kubernetes/tools/cert-manager.tf
@@ -9,6 +9,6 @@ resource "helm_release" "certmanager" {
   chart           = "jetstack/cert-manager"
   version         = "1.18.0"
   repository      = "https://charts.jetstack.io"
-  namespace       = kubernetes_namespace.certmanager.metadata[0].name
+  namespace       = kubernetes_namespace.certificates.metadata[0].name
   cleanup_on_fail = true
 }
