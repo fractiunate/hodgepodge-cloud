78ceddc78581174209049b0eb38bfd17722543c5
Fractiunate// David Rahäuser <david.rahaeuser@gebit.de>
2025-06-16 23:49:37 +0200

    feat: set github environment secrets

---
+++ terraform/kubernetes-tools/dev.gh.secrets.js
@@ -0,0 +1,18 @@
+exports.data = [
+    {
+        name: "b64_cluster_host",
+        mandatory: true
+    },
+    {
+        name: "b64_client_certificate",
+        mandatory: true
+    },
+    {
+        name: "b64_client_key",
+        mandatory: true
+    },
+    {
+        name: "b64_cluster_ca_certificate",
+        mandatory: true
+    },
+]
