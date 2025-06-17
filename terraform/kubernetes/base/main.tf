2f36ab83ff9e96c258d426bcdce164f4f0eb56e6
Fractiunate// David Rahäuser <david.rahaeuser@gebit.de>
2025-06-17 14:25:10 +0200

    fix: 2 node count

--- terraform/kubernetes/base/main.tf
+++ terraform/kubernetes/base/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = var.default_node_pool.vm_size
   }
 
