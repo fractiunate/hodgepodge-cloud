662e3cc305d5147ed8a4b22976e7c3a8499e46bb
Fractiunate// David Rahäuser <david.rahaeuser@gebit.de>
2025-06-17 15:50:21 +0200

    fix: add itscontained chart and istio gw

--- terraform/kubernetes/tools/istio.tf
+++ terraform/kubernetes/tools/istio.tf
@@ -13,22 +13,6 @@ resource "helm_release" "istio_base" {
   namespace       = kubernetes_namespace.istio_system.metadata[0].name
 }
 
-resource "helm_release" "istio_ingress" {
-  name            = "istio-ingressgateway"
-  chart           = "gateway"
-  repository      = "https://istio-release.storage.googleapis.com/charts"
-  version         = "1.26.1"
-  cleanup_on_fail = true
-  namespace       = kubernetes_namespace.istio_system.metadata[0].name
-  set = [
-    {
-      name  = "revision"
-      value = "v1"
-    }
-  ]
-  depends_on = [helm_release.istiod]
-}
-
 
 resource "helm_release" "istiod" {
   name            = "istiod"
@@ -53,3 +37,63 @@ resource "helm_release" "istiod" {
   ]
   depends_on = [helm_release.istio_base]
 }
+
+resource "helm_release" "istio_ingress" {
+  name            = "istio-ingressgateway"
+  chart           = "gateway"
+  repository      = "https://istio-release.storage.googleapis.com/charts"
+  version         = "1.26.1"
+  cleanup_on_fail = true
+  namespace       = kubernetes_namespace.istio_system.metadata[0].name
+  set = [
+    {
+      name  = "revision"
+      value = "v1"
+    }
+  ]
+  depends_on = [helm_release.istiod]
+}
+
+
+resource "helm_release" "istio_gateway" {
+  name            = "default-gateway"
+  chart           = "helm/charts/itscontained/raw"
+  cleanup_on_fail = true
+  namespace       = kubernetes_namespace.istio_system.metadata[0].name
+
+  values = [<<EOF
+apiVersion: networking.istio.io/v1
+kind: Gateway
+metadata:
+  name: gateway
+  namespace: ${kubernetes_namespace.istio_system.metadata[0].name}
+spec:
+  selector:
+    app: istio-ingressgateway
+  servers:
+  - port:
+      number: 80
+      name: http
+      protocol: HTTP
+    hosts:
+    - *.dev.fractiunate.me
+EOF
+  ]
+  depends_on = [helm_release.istio_ingress]
+
+}
+
+
+#     tls:
+#       httpsRedirect: true # sends 301 redirect for http requests
+#   - port:
+#       number: 443
+#       name: https-443
+#       protocol: HTTPS
+#     hosts:
+#     - uk.bookinfo.com
+#     - eu.bookinfo.com
+#     tls:
+#       mode: SIMPLE
+#       serverCertificate: /etc/certs/servercert.pem
+#       privateKey: /etc/certs/privatekey.pem
