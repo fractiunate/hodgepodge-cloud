267a8dc333edb30f6169bc4b45ecd7da169144ab
Fractiunate// David Rahäuser <david.rahaeuser@gebit.de>
2025-06-17 12:53:14 +0200

    fix: minimal argocd resources

--- terraform/kubernetes/tools/argocd.tf
+++ terraform/kubernetes/tools/argocd.tf
@@ -11,4 +11,49 @@ resource "helm_release" "argocd" {
   repository      = "https://argoproj.github.io/argo-helm"
   namespace       = kubernetes_namespace.argocd.metadata[0].name
   cleanup_on_fail = true
+
+  values = [<<EOF
+controller:
+  resources:
+    requests:
+      memory: "128Mi"
+      cpu: "100m"
+    limits:
+      memory: "256Mi"
+      cpu: "200m"
+
+server:
+  resources:
+    requests:
+      memory: "64Mi"
+      cpu: "50m"
+    limits:
+      memory: "128Mi"
+      cpu: "100m"
+
+repoServer:
+  resources:
+    requests:
+      memory: "64Mi"
+      cpu: "50m"
+    limits:
+      memory: "128Mi"
+      cpu: "100m"
+
+dex:
+  enabled: false
+
+notifications:
+  enabled: false
+
+redis:
+  resources:
+    requests:
+      memory: "64Mi"
+      cpu: "25m"
+    limits:
+      memory: "128Mi"
+      cpu: "50m"
+EOF
+  ]
 }
