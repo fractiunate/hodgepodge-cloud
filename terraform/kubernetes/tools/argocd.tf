8143562e4beef799f6fb035ea5f6e08c2bb9cb44
Fractiunate// David Rahäuser <david.rahaeuser@gebit.de>
2025-06-17 09:51:07 +0200

    fix: argo chart

--- terraform/kubernetes/tools/argocd.tf
+++ terraform/kubernetes/tools/argocd.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace" "argocd" {
 
 resource "helm_release" "argocd" {
   name            = "argocd"
-  chart           = "argo/argo-cd"
+  chart           = "argo-cd"
   version         = "8.1.0"
   repository      = "https://argoproj.github.io/argo-helm"
   namespace       = kubernetes_namespace.argocd.metadata[0].name
