b30ed421a896c9a167f257fd6d2f038cc09aff41
Fractiunate// David Rahäuser <david.rahaeuser@gebit.de>
2025-06-18 16:28:29 +0200

    feat: cert-manager federated identity

--- terraform/kubernetes/base/main.tf
+++ terraform/kubernetes/base/main.tf
@@ -10,31 +10,6 @@ resource "azurerm_resource_group" "this" {
   location = var.location
 }
 
-data "azurerm_resource_group" "dns" {
-  provider = azurerm.dns
-  count    = try(var.custom_domain.resource_group_name, null) != null ? 1 : 0
-  name     = var.custom_domain.resource_group_name
-}
-
-resource "azurerm_user_assigned_identity" "aks_workload_identity" {
-  count               = var.workload_identity_enabled ? 1 : 0
-  location            = var.location
-  name                = "${var.stage}aksgitops${random_string.suffix.result}"
-  resource_group_name = azurerm_resource_group.this.name
-  tags                = local.tags
-}
-
-
-resource "azurerm_role_assignment" "dns" {
-  count                = try(var.custom_domain.domain_name, null) != null && var.workload_identity_enabled ? 1 : 0
-  scope                = try(var.custom_domain.resource_group_name, null) == null ? azurerm_resource_group.this.id : data.azurerm_resource_group.dns[0].id
-  role_definition_name = "DNS Zone Contributor"
-  principal_id         = azurerm_user_assigned_identity.aks_workload_identity[0].principal_id
-  depends_on = [
-    azurerm_user_assigned_identity.aks_workload_identity
-  ]
-}
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                      = "${var.stage}-aks-gitops-${random_string.suffix.result}"
   location                  = var.location
